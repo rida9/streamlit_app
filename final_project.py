0077db98c0a8b88d593f118a91c3859ead319fac
Rida94 <63983893+rida9@users.noreply.github.com>
2025-04-23 09:57:59 +0100

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -33,7 +33,7 @@ def parameter(df_sp,sector_default_val,cap_default_val):
 
 @st.cache
 def read_data():
-    path_data = 'final_version/project/s&p500.csv' 
+    path_data = 's&p500.csv' 
     df_sp = pd.read_csv(path_data)
     return df_sp
 
@@ -118,7 +118,7 @@ if __name__ == "__main__":
     st.title('S&P500 Screener & Stock Analysis')
     st.sidebar.title('Search criteria')
 
-    image = Image.open('final_version/project/stock.jpeg')
+    image = Image.open('stock.jpeg')
     yf.pdr_override()
     _, col_image_2,_ = st.columns([1,3,1])
     with col_image_2:
